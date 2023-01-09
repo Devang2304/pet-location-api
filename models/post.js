5cb230afca78a264cdc34757f9f247cd02a90e08
Devang2304 <devangvartak2304@gmail.com>
2023-01-09 20:53:33 +0530

    modified model

--- models/post.js
+++ models/post.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 
-const PostSchema=mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: false
-    },
-    date:{
-        type: Date,
-        default: Date.now
-    }
-    })
+const LocationSchema=mongoose.Schema({
+    location: {
+        type: {
+          type: String,
+          enum: ['Point'],
+          required: true
+        },
+        coordinates: {
+          type: [Number],
+          required: true
+        }
+      }
+    });
+    
 
-    module.exports = mongoose.model('Posts',PostSchema);
+    module.exports = mongoose.model('Posts',LocationSchema);
